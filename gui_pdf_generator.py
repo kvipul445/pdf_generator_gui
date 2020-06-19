74c5c4c9602112ad8dd9537c241fd46316450f22
Varun Kumar <kvipul445@gmail.com>
2020-06-19 23:55:44 +0530

    Added label and next button

--- gui_pdf_generator.py
+++ gui_pdf_generator.py
@@ -2,4 +2,13 @@ from tkinter import *
 
 root = Tk()
 
+main_frame= Frame(root, width=200, height=200)
+main_frame.pack()
+main_label = Label(main_frame, text='PDF File Generator')
+main_label.pack()
+
+bottom_frame = Frame(root)
+bottom_frame.pack(side=BOTTOM)
+next_button = Button(main_frame,text='Next')
+next_button.pack()
 root.mainloop()
